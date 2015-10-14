31262a49f7609d8c74e2320ed055a8cea4f370c3
shawinmihail <shawinmihail@gmail.com>
2015-10-14 22:24:18 +0300

    commit

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import matplotlib
 matplotlib.use("Qt5Agg")
 from core.SystemBuilder import SystemBuilder as SB
 from core.IntervalCalculator import IntervalCalculator as IC
+from gui import PlotPane
+import math
+
+k_list = list()
+v_list = list()
 
 SB.create_random_system()
-print(IC.calculate_total_real_storage_costs(999))
 IC.calculate_and_set_periods()
+SB.change_periods_in(0.125)
 SB.set_period_and_time_in_way_lists()
-print(IC.calculate_total_real_storage_costs(999))
-SB.change_periods_in(2)
-SB.set_period_and_time_in_way_lists()
-print(IC.calculate_total_real_storage_costs(999))
-SB.change_periods_in(0.25)
-SB.set_period_and_time_in_way_lists()
-print(IC.calculate_total_real_storage_costs(999))
+k = math.pow(2, -6)
+for i in range(9):
+    k_list.append(k)
+    v = IC.calculate_total_real_storage_costs(999)
+    v_list.append(v)
+    k *= 2
+    SB.change_periods_in(2)
+    SB.set_period_and_time_in_way_lists()
+
+PlotPane.draw_2d_plot(k_list, v_list)
